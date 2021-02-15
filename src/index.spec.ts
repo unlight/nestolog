4b2885d1d72f7cc3582881e09b95b977f9773618
Roman_Vasilev <s91630277@gmail.com>
2021-02-16 01:21:35 +0400

    smoke test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,10 +2,16 @@ import expect from 'expect';
 
 import { messageColumnWidth } from './message-column-width';
 import { nestologOptionsDefaults } from './nestolog-options.provider';
+import { NestoLogger } from './nestologger.service';
 
 describe('nestoLog', () => {
     it('configure messageColumnWidth temp', () => {
         const result = messageColumnWidth(nestologOptionsDefaults);
         expect(result).not.toBeUndefined();
+    });
+
+    it('smoke', () => {
+        const logger = new NestoLogger(nestologOptionsDefaults!);
+        logger.log('Test message\nOK');
     });
 });
