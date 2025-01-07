acbe19c54fe1c2441870ed361773a79d0f608322
Roman <s91630277@gmail.com>
2025-01-07 23:13:42 +0400

    chore: Remove incorrect build

--- src/nestolog.module-definition.ts
+++ src/nestolog.module-definition.ts
@@ -5,6 +5,4 @@ import { NestologOptions } from './nestolog-options';
 export const { ConfigurableModuleClass, MODULE_OPTIONS_TOKEN } =
   new ConfigurableModuleBuilder<Partial<NestologOptions>>({
     optionsInjectionToken: 'NESTOLOG_OPTIONS',
-  })
-    .setClassMethodName('forRoot')
-    .build();
+  }).build();
